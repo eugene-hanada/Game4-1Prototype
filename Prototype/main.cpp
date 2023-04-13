69d1847254ef0ec712a7efa0aa3554d7871e0b80
eugene-hanada <76219486+eugene-hanada@users.noreply.github.com>
2023-04-13 15:59:54 +0900

    弾が撃てて敵に近づくと追われる

--- Prototype/main.cpp
+++ Prototype/main.cpp
@@ -12,6 +12,7 @@ enum class CharType
 {
 	Player,
 	Enemy,
+	Bullet,
 };
 
 // キャラのベース
@@ -19,7 +20,7 @@ class Character
 {
 public:
 	Character(bool isEnemy, float posX, float posY) :
-		posX_{posX}, posY_{posY}
+		posX_{posX}, posY_{posY}, isAlive_{true}
 	{
 		if (isEnemy)
 		{
@@ -51,6 +52,8 @@ public:
 	{
 		return posY_;
 	}
+
+	virtual void OnHit(CharType type) = 0;
 protected:
 
 	// X座標
@@ -58,12 +61,45 @@ protected:
 
 	// Y座標
 	float posY_;
-private:
 
 	// 色
 	int color_;
 	
-	static constexpr float r_ = 20.0f;
+	float r_ = 20.0f;
+
+	bool isAlive_;
+};
+
+class Bullet :
+	public Character
+{
+public:
+	Bullet(float x, float y, float rot) :
+		Character{ false, x,y }, rot_{ rot }
+	{
+		r_ = 7.0f;
+		color_ = 0xffffff;
+	}
+
+	void Update(CharaList& list) final
+	{
+		posX_ += std::cos(rot_) * 5.0f;
+		posY_ += std::sin(rot_) * 5.0f;
+	}
+private:
+	CharType GetCharType(void)
+	{
+		return CharType::Bullet;
+	}
+
+	void OnHit(CharType type) final
+	{
+		if (type == CharType::Enemy)
+		{
+			isAlive_ = false;
+		}
+	}
+	float rot_;
 };
 
 // プレイヤー
@@ -72,7 +108,7 @@ class Player :
 {
 public:
 	Player() :
-		Character{false,640.0f, 360.0f}, rot_{0.0f}
+		Character{false,640.0f, 360.0f}, rot_{0.0f}, trigger_{false}
 	{
 
 	}
@@ -105,6 +141,18 @@ private:
 		float tmpY = 360.0f - y;
 		rot_ = std::atan2(tmpY, tmpX) + DX_PI;
 
+		if ((GetMouseInput() & MOUSE_INPUT_LEFT))
+		{
+			if (!trigger_)
+			{
+				trigger_ = true;
+				list.emplace_back(std::make_unique<Bullet>(posX_, posY_, rot_));
+			}
+		}
+		else
+		{
+			trigger_ = false;
+		}
 
 	}
 
@@ -124,11 +172,20 @@ private:
 		return CharType::Player;
 	}
 
+	void OnHit(CharType type) final
+	{
+		if (type == CharType::Enemy)
+		{
+			isAlive_ = false;
+		}
+	}
+
 	// 回転角度
 	float rot_;
+	bool trigger_;
 };
 
-
+// 敵
 class Enemy :
 	public Character
 {
@@ -145,17 +202,21 @@ private:
 	void Update(CharaList& list) final
 	{
 		// プレイヤー見つける
-		const auto& player = std::find_if(list.begin(), list.end(), [](std::unique_ptr<Character>& c) {return c->GetCharType() == CharType::Enemy; });
+		const auto& player = std::find_if(list.begin(), list.end(), [](std::unique_ptr<Character>& c) {return c->GetCharType() == CharType::Player; });
 
 		// 距離調べる
 		float x = (*player)->GetX();
 		float y = (*player)->GetY();
-		float length = std::pow(posX_ - x, 2) + std::pow(posY_ - y, 2);
+		float tmpX = x - posX_;
+		float tmpY = y - posY_;
+		float length = std::pow(tmpX, 2) + std::pow(tmpY, 2);
 		
-		if (length <= 70.0f)
+		if (length <= std::pow(200.0f,2))
 		{
 			// 一定の距離以下なら追尾する
-
+			float r = std::atan2(tmpY, tmpX);
+			posX_ += std::cos(r) * 1.5f;
+			posY_ += std::sin(r) * 1.5f;
 		}
 	}
 
@@ -163,7 +224,17 @@ private:
 	{
 		return CharType::Enemy;
 	}
+
+	void OnHit(CharType type) final
+	{
+		if (type == CharType::Bullet)
+		{
+			isAlive_ = false;
+		}
+	}
 };
+
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int mCmdShow)
 {
@@ -175,7 +246,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	CharaList list;
 
 	list.emplace_back(std::make_unique<Player>());
-
+	list.emplace_back(std::make_unique<Enemy>(100.0f, 100.0f));
 	while (!ProcessMessage())
 	{
 		std::for_each(list.begin(), list.end(), [&list](std::unique_ptr<Character>& c) {c->Update(list); });
